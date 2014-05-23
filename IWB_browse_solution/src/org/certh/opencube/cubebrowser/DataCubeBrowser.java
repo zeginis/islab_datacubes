4758d31af800949657a9a8da4dd64be13cf65fa2
zeginis <zeginis@gmail.com>
2014-05-23 18:39:58 +0300

    Create 2D

--- IWB_browse_solution/src/org/certh/opencube/cubebrowser/DataCubeBrowser.java
+++ IWB_browse_solution/src/org/certh/opencube/cubebrowser/DataCubeBrowser.java
@@ -1,7 +1,10 @@
 package org.certh.opencube.cubebrowser;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+
+import javax.swing.plaf.DimensionUIResource;
 
 import com.fluidops.ajax.FClientUpdate;
 import com.fluidops.ajax.components.FButton;
@@ -21,6 +24,7 @@ import com.fluidops.iwb.api.EndpointImpl;
 import com.fluidops.iwb.widget.AbstractWidget;
 import com.fluidops.iwb.api.query.QueryBuilder;
 
+import org.apache.axis.attachments.DimeTypeNameFormat;
 import org.certh.opencube.utils.IWBquery;
 import org.openrdf.query.BindingSet;
 import org.openrdf.query.MalformedQueryException;
@@ -54,7 +58,6 @@ public class DataCubeBrowser extends AbstractWidget<DataCubeBrowser.Config> {
 	protected FComponent getComponent(String id) {
 
 		Config config = get();
-		
 
 		// the layouting container for this widget
 		// the container must use the provided id
@@ -65,35 +68,134 @@ public class DataCubeBrowser extends AbstractWidget<DataCubeBrowser.Config> {
 
 		final FTable ftable = new FTable("ftable");
 
-		
-		String query = "select distinct ?x ?y where"
-				+ "{<http://www.w3.org/TR/2003/PR-owl-guide-20031209/wine#WhiteLoire> ?x ?y}";
-		
-		query="select  distinct ?dim where {"+config.dataCubeURI+" <http://purl.org/linked-data/cube#structure> " +
-				" ?dsd.?dsd <http://purl.org/linked-data/cube#component>  ?cs." +
-				"?cs <http://purl.org/linked-data/cube#dimension>  ?dim.}";
-		
-		//GET CUBE DIMENSIONS THEN SELECT TWO AND SHOW OBSERVATIONS
+		// Get all Cube dimensions
+		List<String> cubeDimensions = IWBquery
+				.getDataCubeDimensions(config.dataCubeURI);
 
-		//Execute select query and get tuples
-		TupleQueryResult res = IWBquery.executeSelect(query);
+		// Get the Cube measure
+		List<String> cubeMeasure = IWBquery
+				.getDataCubeMeasure(config.dataCubeURI);
 
-		//create the table model from result to visualize
-		FTableModel tm = createTableModel(res);
+		// Get Cube dimensions to visualize
+		HashMap<String, List<String>> dimensions4Visualisation = getDimsAndValues4Visualisation(cubeDimensions);
 
+		// Get Cube dimensions with fixed values
+		HashMap<String, List<String>> fixedDimensionValues = getFixedDimensionValues(cubeDimensions);
+
+		// Get query tuples for visualization
+		TupleQueryResult res = IWBquery.get2DVisualsiationValues(
+				dimensions4Visualisation.keySet(), fixedDimensionValues,
+				cubeMeasure, config.dataCubeURI);
+
+		FTableModel tm = createTupleQueryTableModel(res);
+		FTableModel tm2=create2DCubeTableModel(res,dimensions4Visualisation);
 		ftable.setShowCSVExport(true);
 		ftable.setNumberOfRows(20);
 		ftable.setEnableFilter(true);
 		ftable.setOverFlowContainer(true);
 		ftable.setFilterPos(FilterPos.TOP);
 		ftable.setSortable(true);
-		ftable.setModel(tm);
+		ftable.setModel(tm2);
 
 		cnt.add(ftable);
 		return cnt;
 	}
 
-	private FTableModel createTableModel(TupleQueryResult res) {
+	private HashMap<String, List<String>> getDimsAndValues4Visualisation(
+			List<String> allDimensions) {
+
+		HashMap<String, List<String>> dimensionsAndValues = new HashMap<String, List<String>>();
+
+		// If there exist at least 2 dimensions
+		List<String> visualDimensions = null;
+		if (allDimensions.size() > 1) {
+			visualDimensions = allDimensions.subList(0, 2);
+		} else { // If there is only one dimension
+			visualDimensions = allDimensions;
+		}
+
+		for (String vDim : visualDimensions) {
+			List<String> vDimValues = IWBquery.getDimensionValues(vDim);
+			// Select the first value for each dimension
+			dimensionsAndValues.put(vDim, vDimValues);
+		}
+
+		return dimensionsAndValues;
+	}
+
+	private HashMap<String, List<String>> getFixedDimensionValues(
+			List<String> allDimensions) {
+
+		HashMap<String, List<String>> fixedDimensionValues = new HashMap<String, List<String>>();
+
+		// If there are at least 3 dimensions (2 are visualised an 1 fixed)
+		if (allDimensions.size() > 2) {
+			List<String> fixedDimensions = allDimensions.subList(2,
+					allDimensions.size());
+			for (String fixedDim : fixedDimensions) {
+				List<String> dimValues = IWBquery.getDimensionValues(fixedDim);
+				// Select the first value for each dimension
+				fixedDimensionValues.put(fixedDim, dimValues);
+			}
+		}
+
+		return fixedDimensionValues;
+
+	}
+
+	private FTableModel create2DCubeTableModel(TupleQueryResult res,
+			HashMap<String, List<String>> dimensions4Visualisation) {
+
+		List<String> vDimsList=new ArrayList<String>();
+		for(String vDim:dimensions4Visualisation.keySet()){
+			vDimsList.add(vDim);
+		}
+		
+		List<String> dim1=dimensions4Visualisation.get(vDimsList.get(0));
+		List<String> dim2=dimensions4Visualisation.get(vDimsList.get(1));
+		
+		String[][] v2DCube=new String[dim2.size()][dim1.size()];
+		
+		
+		FTableModel tm = new FTableModel();
+
+		List<String> bindingNames;
+		try {
+			bindingNames = res.getBindingNames();
+
+			tm.addColumn(vDimsList.get(1));
+			
+    		for (String dim1Val : dim1) {
+				tm.addColumn(dim1Val);
+			}
+
+			while (res.hasNext()) {
+				BindingSet bindingSet = res.next();
+				int size2 = bindingSet.size();
+				String[] data = new String[3];
+				
+				String dim1Val=bindingSet.getValue(bindingNames.get(0)).stringValue();
+				String dim2Val=bindingSet.getValue(bindingNames.get(1)).stringValue();
+				String measure=bindingSet.getValue(bindingNames.get(2)).stringValue();
+	    		v2DCube[dim2.indexOf(dim2Val)][dim1.indexOf(dim1Val)]=measure;
+			}
+			
+			for(int i=0;i<dim2.size()-1;i++){
+				String[] data = new String[dim1.size()+1];
+				data[0]=dim2.get(i);
+				for(int j=1;j<dim1.size();j++){
+					data[j]=v2DCube[i][j-1];
+				}
+				tm.addRow(data);
+			}
+			System.out.println(v2DCube);
+		} catch (QueryEvaluationException e) {
+			e.printStackTrace();
+		}
+		return tm;
+	}
+
+	private FTableModel createTupleQueryTableModel(TupleQueryResult res) {
 
 		FTableModel tm = new FTableModel();
 
--- IWB_browse_solution/src/org/certh/opencube/utils/IWBquery.java
+++ IWB_browse_solution/src/org/certh/opencube/utils/IWBquery.java
@@ -1,5 +1,11 @@
 package org.certh.opencube.utils;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
+
+import org.openrdf.query.BindingSet;
 import org.openrdf.query.MalformedQueryException;
 import org.openrdf.query.QueryEvaluationException;
 import org.openrdf.query.TupleQuery;
@@ -11,29 +17,122 @@ import com.fluidops.iwb.api.ReadDataManager;
 import com.fluidops.iwb.api.query.QueryBuilder;
 
 public class IWBquery {
-	
-	
-	public static TupleQueryResult executeSelect(String query){
-	ReadDataManager dm = EndpointImpl.api().getDataManager();
-	QueryBuilder<TupleQuery> queryBuilder = QueryBuilder.createTupleQuery(query);
+
+	public static TupleQueryResult executeSelect(String query) {
+		ReadDataManager dm = EndpointImpl.api().getDataManager();
+		QueryBuilder<TupleQuery> queryBuilder = QueryBuilder
+				.createTupleQuery(query);
 
 		TupleQueryResult res = null;
-	
+
 		try {
 			TupleQuery tulpequery = queryBuilder.build(dm);
-			 res = tulpequery.evaluate();
+			res = tulpequery.evaluate();
 		} catch (MalformedQueryException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (RepositoryException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (QueryEvaluationException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	 
+
 		return res;
+	}
+
+	public static List<String> getDataCubeDimensions(String dataCubeURI) {
+
+		String getCubeDimensions_query = "select  distinct ?dim where {"
+				+ dataCubeURI+ " <http://purl.org/linked-data/cube#structure> ?dsd."
+				+ "?dsd <http://purl.org/linked-data/cube#component>  ?cs."
+				+ "?cs <http://purl.org/linked-data/cube#dimension>  ?dim.}";
+
+		TupleQueryResult res = executeSelect(getCubeDimensions_query);
+
+		List<String> cubeDimensions = new ArrayList<String>();
+
+		try {
+			while (res.hasNext()) {
+				BindingSet bindingSet = res.next();
+				cubeDimensions.add(bindingSet.getValue("dim").stringValue());
+			}
+		} catch (QueryEvaluationException e) {
+			e.printStackTrace();
+		}
+		return cubeDimensions;
+	}
+	
+	public static List<String> getDataCubeMeasure(String dataCubeURI) {
+
+		String getCubeDimensions_query = "select  distinct ?dim where {"
+				+ dataCubeURI+ " <http://purl.org/linked-data/cube#structure> ?dsd."
+				+ "?dsd <http://purl.org/linked-data/cube#component>  ?cs."
+				+ "?cs <http://purl.org/linked-data/cube#measure>  ?dim.}";
+
+		TupleQueryResult res = executeSelect(getCubeDimensions_query);
+
+		List<String> cubeMeasure = new ArrayList<String>();
+
+		try {
+			while (res.hasNext()) {
+				BindingSet bindingSet = res.next();
+				cubeMeasure.add(bindingSet.getValue("dim").stringValue());
+			}
+		} catch (QueryEvaluationException e) {
+			e.printStackTrace();
+		}
+		return cubeMeasure;
+	}
+	
+	public static List<String> getDimensionValues(String dimensionURI) {
+
+		String getDimensionValues_query = "select  distinct ?value where {"
+				+ "?observation <"+ dimensionURI+"> ?value.}";
+		
+		TupleQueryResult res = executeSelect(getDimensionValues_query);
+
+		List<String> dimensionValues = new ArrayList<String>();
+
+		try {
+			while (res.hasNext()) {
+				BindingSet bindingSet = res.next();
+				dimensionValues.add(bindingSet.getValue("value").stringValue());
+			}
+		} catch (QueryEvaluationException e) {
+			e.printStackTrace();
+		}
+		return dimensionValues;
+	}
+	
+	public static TupleQueryResult get2DVisualsiationValues(Set<String> visualDims,
+			HashMap<String,List<String>> fixedDims, List<String> measures, String cubeURI){
+		
+		String sparql_query="Select ";
+		int i=1;
+		//Add variables ?dim to SPARQL query
+		for(String vDim:visualDims){
+			sparql_query+="?dim"+i+" ";
+			i++;
+		}
+		
+		//Select observations of a specific cube (cubeURI)
+		sparql_query+="?measure where{ ?obs <http://purl.org/linked-data/cube#dataSet> "+cubeURI+".";
+		
+        i=1;
+        //Add free dimensions to where clause
+		for(String vDim:visualDims){
+					sparql_query+="?obs <"+vDim+"> "+"?dim"+i+". ";
+					i++;
+		}		
+		 
+		//Add fixed dimensions to where clause, select the first value of the list of all values
+		for(String fDim:fixedDims.keySet()){
+			sparql_query+="?obs <"+fDim+"> <"+fixedDims.get(fDim).get(0)+">.";
+		}
+		
+	   	sparql_query+="?obs  <"+measures.get(0)+"> ?measure}";
+	   	System.out.println(sparql_query);
+	   	return executeSelect(sparql_query);
+		
 	}
 
 }
